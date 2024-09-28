c21f7905616f0cf46333e22a9001784f0cad2ea0
Yared Mekonnen <yaredmekonnen999@gmail.com>
2024-09-28 15:21:11 -0500

    Optimized two sum problem
    In this implementation I've used a hashmap to optimize the time complexity from O(n^2) to O(n). This affects it's space complexity from O(1) to O(n) but it's more optimized.

---
+++ DSandALGO/TwoSumHashMap.js
@@ -0,0 +1,14 @@
+function findPair(nums, targe){
+    const numMap = new Map();
+    const target = targe;
+
+    for(let i = 0; i < nums.length; i++){
+        let complement = target - nums[i];
+
+        if(numMap.has(complement)){
+            return [complement, nums[i]];
+        }
+
+        numMap.set(nums[i], i);
+    }
+}
