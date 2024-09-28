ba430de670e85f3ea588a228d562f3522d744faf
Yared Mekonnen <yaredmekonnen999@gmail.com>
2024-09-28 00:03:09 -0500

    binary search
    Implementing binary search with a sorted array.

---
+++ DSandALGO/binarySearch.js
@@ -0,0 +1,18 @@
+function binarySearch(sortedArray, target){
+    let start = 0;
+    let end = sortedArray.length - 1;
+
+    while(start <= end){
+        let mid = Math.floor((start + end) / 2);
+
+        if(sortedArray[mid] === target){
+            return mid;
+        }else if(sortedArray[mid] < target){
+            start = mid + 1;
+        }else{
+            end = mid - 1;
+        }
+
+        return -1;
+    }
+}
