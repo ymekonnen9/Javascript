aadd2277c1aea6c8db219b8db97e030d45280db1
Yared Mekonnen <yaredmekonnen999@gmail.com>
2024-11-02 15:06:45 -0500

    Quick Sort
    Quick sort implementation

---
+++ Advanced/divideAndConqure/quickSort.js
@@ -0,0 +1,24 @@
+function quickSort(arra){
+    let pivot = arra[0];
+    let left = 1;
+    let right = arra.length - 1;
+
+    while(left <= right){
+
+        while(left <= right && arra[right] > pivot){
+            right--;
+        }
+
+        while(left <= right && arra[left] < pivot){
+            left ++;
+        }
+
+        while(left < right){
+            [arra[left], arra[right]] = [arra[right], arra[left]];
+        }
+    }
+
+    [arra[0], arra[right]] = [arra[right], arra[0]];
+
+    return arra;
+}
